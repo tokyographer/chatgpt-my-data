f182ce03f3a4eddd6d433d7977875d83ac092821
Anthony T <tokyographer@gmail.com>
2023-07-14 12:33:23 +0200

    Updated file with new functionality: from flask import Flask, request, render_template from chatgpt import run_chat_gpt  # Import the function from your script
    app = Flask(__name__)
    
    @app.route('/', methods=['GET', 'POST'])
    def home():
        if request.method == 'POST':
            loader_path = request.form.get('loader')  # Get loader path from form
            response = run_chat_gpt(loader_path)  # Run your chatgpt script with the loader path
            return render_template('index.html', response=response)  # Display the result on the page
        return render_template('index.html')
    
    if __name__ == '__main__':
        app.run(debug=True)

--- chatgpt.py
+++ chatgpt.py
@@ -18,35 +18,38 @@ os.environ["OPENAI_API_KEY"] = constants.APIKEY
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
 PERSIST = False
 
-query = None
-if len(sys.argv) > 1:
-  query = sys.argv[1]
 
-if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-  loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+def run_chat_gpt(loader_path):
+    query = None
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
 
-chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    else:
+        loader = TextLoader(loader_path)
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
+        else:
+            index = VectorstoreIndexCreator().from_loaders([loader])
 
-chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
 
-  chat_history.append((query, result['answer']))
-  query = None
+    chat_history = []
+    while True:
+        if not query:
+            query = input("Prompt: ")
+        if query in ['quit', 'q', 'exit']:
+            sys.exit()
+        result = chain({"question": query, "chat_history": chat_history})
+        print(result['answer'])
+
+        chat_history.append((query, result['answer']))
+        query = None
+
+    return result
